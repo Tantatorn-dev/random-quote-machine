2b276f46e5c9a6c790808d3fd8d1ac4ee8f01f98
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-02-06 21:38:22 +0700

    add SemanticUI.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header, Container, Grid } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ background: 'DarkSalmon', height: "100vh" }}>
+
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            
+              <Header as='h1'>Hello</Header> 
+           
+          </Grid.Column>
+        </Grid>
+
+
       </div>
     );
   }
