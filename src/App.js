5da8b1bc7c8e10e6bb1566ecc0a47ff31ea73f05
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-02-07 09:15:57 +0700

    Add the cool transition to the text

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Header, Container, Grid } from 'semantic-ui-react'
+import {  Grid, Button ,Transition} from 'semantic-ui-react'
+
+
+const quotes = ['I love dog', 'I love cat', 'I love Coca Cola','I love pepsi','google spaghetti'];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_quote_index: 0,
+      visible:true
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.onAnimationComplete= this.onAnimationComplete.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+
+  onAnimationComplete(){
+    this.setState({
+      current_quote_index: this.state.current_quote_index + 1,
+      visible: !this.state.visible
+    })
+  }
+
   render() {
     return (
       <div style={{ background: 'DarkSalmon', height: "100vh" }}>
 
         <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
           <Grid.Column style={{ maxWidth: 450 }}>
-            
-              <Header as='h1'>Hello</Header> 
-           
+
+            <Transition  onHide={this.onAnimationComplete} visible={this.state.visible} animation='scale' duration={1000}>
+              <p style={{ color: 'white', fontSize: 28 }}>{quotes[this.state.current_quote_index]}</p>
+            </Transition>
+
+            <Button onClick={this.handleClick} content='New Quote' inverted color='standard' />
+
           </Grid.Column>
         </Grid>
 
