edcb99914adc3ba87ca0252cd5a7e1065c4b44be
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-02-07 20:34:49 +0700

    The app can display the quote and change the quote when we click

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {  Grid, Button ,Transition} from 'semantic-ui-react'
+import { Grid, Button, Transition } from 'semantic-ui-react'
 
 
 
@@ -8,29 +8,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes:[],
+      authors: [],
+      quotes: [],
       current_quote_index: 0,
-      visible:true
+      visible: true
     }
     this.handleClick = this.handleClick.bind(this);
-    this.onAnimationComplete= this.onAnimationComplete.bind(this);
+    this.onAnimationComplete = this.onAnimationComplete.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
     fetch('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=40')
-    .then(res =>{
-      return res.json();
-    }).then(data=>{
-      let quote=[];
-      for(let i=0;i<40;i++){
-        quote=[...quote,data[i].title];
-      }
-      this.setState({
-        quotes: quote
+      .then(res => {
+        return res.json();
+      }).then(data => {
+        let temp_quote = [];
+        let temp_author = [];
+
+        for (let i = 0; i < 40; i++) {
+          let text = data[i].content;
+          let cleanText = text.replace(/<\/?[^>]+(>|$)/g, "");
+          temp_quote = [...temp_quote, cleanText];
+          temp_author = [...temp_author, data[i].title];
+        }
+        this.setState({
+          quotes: temp_quote,
+          authors: temp_author
+        })
       })
-    })
-    
+
 
   }
 
@@ -40,7 +47,7 @@ class App extends Component {
     })
   }
 
-  onAnimationComplete(){
+  onAnimationComplete() {
     this.setState({
       current_quote_index: this.state.current_quote_index + 1,
       visible: !this.state.visible
@@ -48,14 +55,22 @@ class App extends Component {
   }
 
   render() {
+    let someHtml=this.state.quotes[this.state.current_quote_index];
     return (
       <div style={{ background: 'DarkSalmon', height: "100vh" }}>
 
         <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
           <Grid.Column style={{ maxWidth: 450 }}>
 
-            <Transition  onHide={this.onAnimationComplete} visible={this.state.visible} animation='scale' duration={1000}>
-              <p style={{ color: 'white', fontSize: 28 }}>{this.state.quotes[this.state.current_quote_index]}</p>
+            
+
+            <Transition onHide={this.onAnimationComplete} visible={this.state.visible} animation='scale' duration={1000}>
+            <figure>
+              <div className='quote' 
+              dangerouslySetInnerHTML={{__html:someHtml}} 
+              style={{color: 'white', fontSize: 24}}/>
+              <figcaption style={{color: 'white', fontSize: 22,paddingTop:20}}>{'  - '+this.state.authors[this.state.current_quote_index]}</figcaption>
+            </figure>
             </Transition>
 
             <Button onClick={this.handleClick} content='New Quote' inverted color='standard' />
